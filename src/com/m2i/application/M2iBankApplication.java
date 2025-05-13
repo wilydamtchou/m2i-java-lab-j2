7b78ea17361989c87d247ac65359d2720253ad47
unknown <willydamtchou@gmail.com>
2025-05-13 23:16:17 +0200

    dev

--- src/com/m2i/application/M2iBankApplication.java
+++ src/com/m2i/application/M2iBankApplication.java
@@ -2,7 +2,7 @@ package com.m2i.application;
 
 public class M2iBankApplication {
 	public static void main (String[] args) {
-		System.out.print("Welcome to M2I Bank....");
+		System.out.print("Welcome to M2I Bank...");
 	}
 
 }
